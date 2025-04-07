20b47e47bcb73c5b4d51834652ca86838cee2d35
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-04-08 08:06:55 +0900

    ジェネレーターの学習完了

---
+++ lesson4/c4_2_21.py
@@ -0,0 +1,29 @@
+def counter(num=10):
+    for _ in range(num):
+        yield 'run'
+
+
+def greeting():
+    yield 'Good morning'
+    yield 'Good afternoon'
+    yield 'Good night'
+
+
+g = greeting()
+c = counter()
+
+print(next(g))
+print(next(c))
+print(next(c))
+print(next(c))
+print(next(c))
+print(next(c))
+
+print(next(g))
+print(next(c))
+print(next(c))
+print(next(c))
+print(next(c))
+print(next(c))
+
+print(next(g))
