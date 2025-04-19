6c6565b507a71a918388c8c3b96c61bb9c0fd3a3
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-04-19 20:07:11 +0900

    lesson10の学習完了

--- lesson10/c10_3_2.py
+++ lesson10/c10_3_2.py
@@ -1,4 +1,6 @@
 from email import message
+from email.mime import multipart
+from email.mime import text
 import smtplib
 
 smtp_host = 'smtp.gmail.com'
@@ -9,11 +11,19 @@ to_email = 'hogehoge@gmail.com'
 username = 'hogehoge@gmail.com'
 password = 'urgpgscufecyhoeh'
 
-msg = message.EmailMessage()
-msg.set_content('Test email')
+msg = multipart.MIMEMultipart()
 msg['Subject'] = 'Test email sub'
 msg['From'] = from_email
 msg['To'] = to_email
+msg.attach(text.MIMEText('Test email', 'plain'))
+
+with open('lesson.py', 'r') as f:
+    attachment = text.MIMEText(f.read(), 'plain')
+    attachment.add_header(
+        'Content-Disposition', 'attachment',
+        filename='lesson.txt'
+    )
+    msg.attach(attachment)
 
 server = smtplib.SMTP(smtp_host, smtp_port)
 server.ehlo()
