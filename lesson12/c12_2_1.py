24db192dc08b00d097da28c41c4fa5e9b0df0552
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-04-20 23:19:31 +0900

    マルチプロセスのサンプルを追加

---
+++ lesson12/c12_2_1.py
@@ -0,0 +1,28 @@
+import logging
+import threading
+from tkinter import N
+
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(threadName)s: %(message)s'
+)
+
+
+def worker1(i):
+    logging.debug('start')
+    logging.debug(i)
+    logging.debug('end')
+
+
+def worker2(i):
+    logging.debug('start')
+    logging.debug(i)
+    logging.debug('end')
+
+
+if __name__ == '__main__':
+    i = 10
+    t1 = threading.Thread(target=worker1, args=(i,))
+    t2 = threading.Thread(name='renamed worker2', target=worker2, args=(i,))
+    t1.start()
+    t2.start()
