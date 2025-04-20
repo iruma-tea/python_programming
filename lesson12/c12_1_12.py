0af3605f4e932984a78affd9acbdd564702a0dfe
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-04-20 18:01:56 +0900

    c12_1_17まで学習完了

---
+++ lesson12/c12_1_12.py
@@ -0,0 +1,24 @@
+import logging
+import threading
+import time
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(threadName)s: %(message)s'
+)
+
+
+def worker1():
+    logging.debug('start')
+    time.sleep(5)
+    logging.debug('end')
+
+
+def worker2():
+    logging.debug('start')
+    time.sleep(2)
+    logging.debug('end')
+
+
+if __name__ == '__main__':
+    t = threading.Timer(3, worker1)
+    t.start()
