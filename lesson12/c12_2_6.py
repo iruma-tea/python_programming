0100b3c01c23f8921b33641d75f93abe1039eb10
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-04-21 08:43:00 +0900

    マルチプロセスによる並列化の学習完了

---
+++ lesson12/c12_2_6.py
@@ -0,0 +1,28 @@
+import logging
+import multiprocessing
+
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(processName)s: %(message)s'
+)
+
+
+def worker1(i):
+    logging.debug('start')
+    logging.debug(i)
+    logging.debug('end')
+
+
+def worker2(i):
+    logging.debug('start')
+    logging.debug(i)
+    logging.debug('end')
+
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(1) as p:
+        p1 = p.apply_async(worker1, (100,))
+        p2 = p.apply_async(worker1, (100,))
+        logging.debug('executed')
+        logging.debug(p1.get())
+        logging.debug(p2.get())
